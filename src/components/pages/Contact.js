4e2c7f35c131557e416f9d5d00a5029c84ebcb9e
alotfey <me@alotfey.com>
2022-02-26 14:00:18 -0500

    add form control to contact

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,8 +2,28 @@ import React from 'react';
 
 function Contact() {
   return (
-    <div>
-      <h2>Contact</h2>
+    <div className="container">
+      <form>
+      <div className="mb-3">
+            <label htmlFor="inputName">Name:</label>
+            <input type="text" className='form-control' />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="inputEmail" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="inputEmail"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     </div>
   );
 }
